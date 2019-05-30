6c54e9866d7f65b6a5529b7edc14f0919829275d
Demyanov Vyacheslav <demyanovs@gmail.com>
2019-05-30 22:14:11 +0300

    Code style fix

--- src/HighlighterXML.php
+++ src/HighlighterXML.php
@@ -26,19 +26,19 @@ class HighlighterXML extends HighlighterBase
         // Brackets
         $text = preg_replace(
             '#&lt;([/]*?)(.*)([\s]*?)&gt;#sU',
-            '<span style=\"color: ' . $this->_theme::getXMLTagColor() . '\">&lt;\\1\\2\\3&gt;</span>',
+            '<span style="color: ' . $this->_theme::getXMLTagColor() . '">&lt;\\1\\2\\3&gt;</span>',
             $text
         );
         // Xml version
         $text = preg_replace(
             '#&lt;([\?])(.*)([\?])&gt;#sU',
-            '<span style=\"color: ' . $this->_theme::getXMLInfoColor() . '\">&lt;\\1\\2\\3&gt;</span>',
+            '<span style="color: ' . $this->_theme::getXMLInfoColor() . '">&lt;\\1\\2\\3&gt;</span>',
             $text
         );
         // Attributes
         $text = preg_replace(
             "#([^\s]*?)\=(&quot;|')(.*)(&quot;|')#isU",
-            '<span style=\"color: ' . $this->_theme::getXMLAttrNameColor() . '\">\\1</span>=<span style=\"color: ' . $this->_theme::getXMLAttrValueColor() . '\">\\2\\3\\4</span>',
+            '<span style="color: ' . $this->_theme::getXMLAttrNameColor() . '">\\1</span>=<span style="color: ' . $this->_theme::getXMLAttrValueColor() . '">\\2\\3\\4</span>',
             $text
         );
 
